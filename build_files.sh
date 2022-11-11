7e5ca2190e6aecc6c1e61d5f713f532b112762e3
Alakesh Sarma <icvc.a10@gmail.com>
2022-11-11 11:33:19 +0530

    buildFiles

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-# build_files.sh
-#pip install virtualenv
-#virtualenv venv 
-#source venv/bin/activate
+#build_files.sh
+pip install virtualenv
+virtualenv venv 
+source venv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic
